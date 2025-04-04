d306a5f9b72470c38471b952e2ed0392d81617eb
Luiz Fellipe Carneiro <fellipec@outlook.com>
2025-04-03 21:02:11 -0300

    1st time working on custom circuit board

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,26 @@
 #include <Arduino.h>
-#include <LiquidCrystal.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 #include <ESP8266WiFi.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <time.h>
-#include <DHT.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <RTClib.h>
 #include <wifipasswd.h>  // Contém as credenciais do Wi-Fi
 
-// Pinos do LCD Keypad Shield no ESP8266
-#define LCD_RS D2
-#define LCD_EN D3
-#define LCD_D4 D4
-#define LCD_D5 D5
-#define LCD_D6 D6
-#define LCD_D7 D7
+Adafruit_BME280 bme; // I2C
+LiquidCrystal_I2C lcd(0x27,20,4);  
 
-// Inicializa o LCD (sem I2C)
-LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+// Rotary encoder pins
+#define CLK D6
+#define DT  D5
+#define SW  D7
+#define SEALEVELPRESSURE_HPA (1013.25)
 
-// Pinos do sensor DHT22
-#define DHTPIN D8
-#define DHTTYPE DHT22
-DHT dht(DHTPIN, DHTTYPE);
+// RTC
+RTC_DS1307 rtc;
 
 // Fuso horário (UTC-3)
 const long utcOffsetInSeconds = -10800;
@@ -37,7 +36,7 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, ntpServers[ntpServerIndex], utcOffsetInSeconds, 60000);
 
 // Dias da semana
-const char* daysOfTheWeek[7] = {"Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"};
+const char* daysOfTheWeek[7] = {"Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"};
 
 // Variáveis globais
 int day, month, year, hour, minute, dayOfWeek;
@@ -74,11 +73,34 @@ void getDateFromEpoch(time_t epoch, int &day, int &month, int &year) {
     year = timeinfo.tm_year + 1900;
 }
 
+
+int counter = 0;
+int lastStateCLK;
+bool btnToggle = false;
+bool lastButtonState = false;
+bool btnPressed = false;
+unsigned long lastMillis = 0;
+unsigned long lastDebounceTime = 0;
+const unsigned long debounceDelay = 50; // 50ms para debounce
+bool status;
+
+
 void setup() {
+    Wire.begin();
     Serial.begin(115200);
-    lcd.begin(16, 2);
+    lcd.begin(20, 4);
     lcd.setCursor(0, 0);
+    lcd.backlight();
     lcd.print("Conectando...");
+    bme.begin(0x76); // I2C address 0x76
+    rtc.begin();
+    Serial.println(rtc.now().timestamp());
+
+    pinMode(CLK, INPUT);
+    pinMode(DT, INPUT);
+    pinMode(SW, INPUT);
+
+    lastStateCLK = digitalRead(CLK);
 
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     int i = 0;
@@ -101,47 +123,108 @@ void setup() {
     lcd.print("Wi-Fi OK!");
 
     timeClient.begin();
-    dht.begin();
+    lcd.setCursor(0, 0);
+    lcd.print("Linha 0");
+    lcd.setCursor(0, 1);
+    lcd.print("Linha 1");
+    lcd.setCursor(0, 2);
+    lcd.print("Linha 2");
+    lcd.setCursor(0, 3);
+    lcd.print("Linha 3");
+    delay(2000);
+
 }
+
+
 
 void loop() {
-    if (lastHour == -1) {
-        lcd.clear();
-    }
-    checkNtpFallback();
+    
+    // Rotary encoder read
+    
 
-    hour = timeClient.getHours();
-    minute = timeClient.getMinutes();
-    getDateFromEpoch(timeClient.getEpochTime(), day, month, year);
-    dayOfWeek = timeClient.getDay();
-
-    float temp = dht.readTemperature();
-    float hum = dht.readHumidity();
-
-    if (isnan(temp) || isnan(hum)) {
-        Serial.println("Erro ao ler DHT22");
-    } else {
-        if (temp != lastTemp || hum != lastHum) {
-            lcd.setCursor(0, 1);
-            lcd.printf("T:%5.1fC H:%5.1f%%  ", temp, hum);
-            lastTemp = temp;
-            lastHum = hum;
+    int currentStateCLK = digitalRead(CLK);
+    // Detecta mudança no CLK
+    if (currentStateCLK != lastStateCLK) {
+        if (digitalRead(DT) != currentStateCLK) {
+            counter++; // Gira para a direita
+        } else {
+            counter--; // Gira para a esquerda
         }
     }
+    lastStateCLK = currentStateCLK;
 
-    if (hour != lastHour || minute != lastMinute || day != lastDay || month != lastMonth || dayOfWeek != lastDayOfWeek) {
-        lcd.setCursor(0, 0);
-        lcd.printf("%02d:%02d  %s %02d/%02d  ", hour, minute, daysOfTheWeek[dayOfWeek], day, month);
+    // Encoder button read
 
-        lastHour = hour;
-        lastMinute = minute;
-        lastDay = day;
-        lastMonth = month;
-        lastDayOfWeek = dayOfWeek;
+    bool currentButtonState = digitalRead(SW);
+    if (currentButtonState == LOW && lastButtonState == HIGH) {
+        btnPressed = true;
+        btnToggle = !btnToggle;
+        Serial.println("Botão pressionado!");
     }
+    lastButtonState = currentButtonState;
 
-    Serial.printf("Hora: %02d:%02d | Data: %02d/%02d | Temp: %.1f°C | Umid: %.1f%%\n", 
-                  hour, minute, day, month, temp, hum);
+    // Main tasks every second
+    if (millis() - lastMillis > 1000) {
+        lastMillis = millis();
+
+        lcd.setCursor(0, 1);
+        lcd.printf("Pos: %d Btn: %s    ", counter, btnToggle ? "ON" : "OFF");
+
+
+        if (lastHour == -1) {
+            lcd.clear();
+        }
+        checkNtpFallback();
     
-    delay(5000);
+        hour = timeClient.getHours();
+        minute = timeClient.getMinutes();
+        getDateFromEpoch(timeClient.getEpochTime(), day, month, year);
+        dayOfWeek = timeClient.getDay();
+    
+    
+        if (hour != lastHour || minute != lastMinute || day != lastDay || month != lastMonth || dayOfWeek != lastDayOfWeek) {
+            lcd.setCursor(0, 0);
+            lcd.printf("%02d:%02d  %s %02d/%02d  ", hour, minute, daysOfTheWeek[dayOfWeek], day, month);
+    
+            lastHour = hour;
+            lastMinute = minute;
+            lastDay = day;
+            lastMonth = month;
+            lastDayOfWeek = dayOfWeek;
+        }
+    
+        Serial.printf("Hora: %02d:%02d | Data: %02d/%02d | Temp: %.1f°C | Umid: %.1f%%\n", 
+                      hour, minute, day, month);
+    
+        Serial.print("Temperature = ");
+        Serial.print(bme.readTemperature());
+        Serial.println(" °C");
+    
+        Serial.print("Pressure = ");
+    
+        Serial.print(bme.readPressure() / 100.0F);
+        Serial.println(" hPa");
+    
+        Serial.print("Approx. Altitude = ");
+        Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+        Serial.println(" m");
+    
+        Serial.print("Humidity = ");
+        Serial.print(bme.readHumidity());
+        Serial.println(" %");
+    
+        Serial.println();
+
+        lcd.setCursor(0, 2);
+        lcd.printf("T: %.1fC  ", bme.readTemperature());
+        lcd.setCursor(9, 2);
+        lcd.printf("U: %.1f%%  ", bme.readHumidity());
+        lcd.setCursor(0, 3);
+        lcd.printf("P: %.1fhPa  ", bme.readPressure() / 100.0F);
+
+        btnPressed = false; // Reset button state
+
+        
+    }
 }
+
